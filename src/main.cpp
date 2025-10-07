a417a571b530ee4f517da16347966e603a9c7a98
Roshan2540 <slamknat@gmail.com>
2025-10-07 12:48:01 +0100

    update print statement

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,6 @@
 int main ()
 {
     // we want to print "My C Compiler is working!" instead
-    std::cout << "Change me before you submit the assignment!";
+    //std::cout << "Change me before you submit the assignment!" << std::endl;
+    std::cout << "My C Compiler is working!";
 }
